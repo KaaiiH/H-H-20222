4a9f5d29f3d5d5844878ecfce8e22c007f768cb2
Kai <kaihoenshell1@gmail.com>
2022-02-25 16:03:21 -0500

    removed hw

--- KaiJaranHW2/HW2/wheel.cpp
+++
@@ -1,6 +0,0 @@
-#include "wheel.h"
-
-// returns a random value from the values vector held in wheel
-auto Roulette::Wheel::spin() -> std::int32_t {
-    return this->values[std::rand() % this->values.size()];
-};
