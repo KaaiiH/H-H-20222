7b07b31ab31f15c67697b6f483a07ba6047a0711
Kai Kai <kaihoenshell1@gmail.com>
2022-02-28 06:33:34 -0500

    have it starting at welcome screen

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,3 +1,5 @@
+const { ok } = require("assert");
+
 /**
  * SmoothScroll
  * This helper script created by DWUser.com.  Copyright 2013 DWUser.com.  
@@ -33,29 +35,23 @@ a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b(
 jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
 
 function openCity(evt, cityName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
-
-    // if(document.getElementById(cityName).style.display = "block"){
-    //   document.getElementByClassName("first-tab").style.display = "block";
-    // }
   
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
+
   }
+
 
 function bracket_open(obj){
   //declares variables
